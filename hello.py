eb9cdee4a79d453481232893c5e8c3fa2e5b41bd
MIh <muravjov.m@gmail.com>
2017-04-07 23:59:10 +0300

    edit gitignore

--- hello.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-
-def app(environ, start_response):
-    query_str = environ.get('QUERY_STRING', '')
-    param_list = [i.encode() + '\n'.encode() for i in query_str.split('&')]
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return param_list
