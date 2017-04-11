c698e8e42d251435b22c1afdb05c2af331b81efc
MIh <muravjov.m@gmail.com>
2017-04-11 22:16:45 +0300

    add secret key to file and change settings.py

--- src/ask/ask/settings.py
+++ src/ask/ask/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+with open(os.path.join(BASE_DIR, 'secretkey.txt')) as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
